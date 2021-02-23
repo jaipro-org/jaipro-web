ca0e0008cba4faeb95b21d1045a4722b718ff3dc
Valeria Cuellar Portella <valeria.cuellar@itsight.pe>
2021-02-22 20:42:29 -0500

    vrc

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,5 @@ module.exports = {
             filename: "[name].css",
             chunkFilename: "css/[name].css",
         },
-    },
-    devServer: {
-        port: process.env.VUE_APP_PORT,
-        disableHostCheck: true,
-        host: "127.0.0.1"
-    },
+    }
 };
