600b00268199be52289fb7594faffd37f18baaac
Jesner <jesner631@gmail.com>
2023-04-08 20:30:05 -0500

    HEP-63: Forgot Password - Update: Modularizing vee-validate with YUP

---
+++ src/validate/forgotPasswordValidate.ts
@@ -0,0 +1,35 @@
+import { useField, configure } from "vee-validate";
+import * as yup from "yup";
+
+// Configura VeeValidate
+configure({
+  validateOnInput: true,
+  validateOnChange: true,
+});
+
+export default function useForgotPasswordValidate() {
+  // Define validación con Yup
+  const emailSchema = yup
+    .string()
+    .email("Escriba un correo valido")
+    .required("Campo requerido")
+
+  // Define los campos de formulario validados
+  const email = useField("email", emailSchema);
+
+  // Define la función de validación
+  async function validate(data: object) {
+    const valideSchema = yup.object({
+      email: emailSchema,
+    });
+
+    const isValid = await valideSchema.isValid(data);
+
+    return isValid
+  }
+
+  return {
+    email,
+    validate,
+  };
+}
